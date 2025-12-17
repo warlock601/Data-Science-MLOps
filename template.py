2f995265dea5fcde02fdaaed542d81289331089f
Vivek Raj <vivek.raj@talentica.com>
2025-12-17 11:51:13 +0530

    Code logic for iterating file-path added

--- template.py
+++ template.py
@@ -26,3 +26,20 @@ list_of_files=[
     "templates/index.html"
 
 ]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir, filename=os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory {filedir} for the file : {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists")
