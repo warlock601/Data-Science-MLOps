4ec1f40b93534743cfc2980fc17510d598e6fac7
Vivek Raj <vivek.raj@talentica.com>
2025-12-17 07:35:08 +0530

    template.py created to define the project structure like what are all the files need to be created

---
+++ template.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+import logging
+
+project_name="DataScienceMLops"
+
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/__common__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "Dockerfile",
+    
+]
