84e0aeb1d6ed828c2f165160bf4bee281e623dfc
Vivek Raj <vivek.raj@talentica.com>
2025-12-18 14:07:16 +0530

    Logic added for data ingestion pipeline:

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.DataScienceMLops import logger
+from src.DataScienceMLops.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging data science")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
